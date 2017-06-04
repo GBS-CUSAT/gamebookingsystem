f7b76b1a301ec82045f6275a6d88f9342b35b78e
krishna7631 <krishna7631@gmail.com>
2017-06-05 02:10:55 +0530

    Update and rename user-registration.html to user-registration.php

---
+++ user-registration.php
@@ -0,0 +1,89 @@
+<?php
+$newrec='';
+if (isset($_POST['submitted'])){
+    include('connect-mysql.php');
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $emailid=$_POST['emailid'];
+    $empid=$_POST['empid'];
+    $phone=$_POST['phone'];
+    $dob=$_POST['dob'];
+    $password=md5($_POST['password']);
+    $isadmin=isset($_POST['isadmin']);
+    $sqlinsert="INSERT INTO users (fname,lname,emp_id,dob,phone,email_id,password,is_admin) VALUES ('$fname','$lname','$empid','$dob','$phone','$emailid','$password','$isadmin')";
+
+if(!mysqli_query($dbcon, $sqlinsert)){
+    die('error');
+}
+$newrec = "request sumbitted";
+
+}//end of main if loop
+?>
+<DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
+<title>registration</title>
+<link rel="stylesheet" type="text/css" href="user-registration.css" />
+</head>
+<body>
+
+<div class="header">
+    <div class="logo" style="float: left;">
+    GAME BOOKING SYSTEM
+    </div>
+    
+        <div class="login" style="float: right;">
+        <ul>
+        <form action="">
+        <li>
+        Employee id/E-mail id<br>
+        <input type="text" name="name"></li> 
+        <li>Password<br>
+        <input type="password" name="password"/></li>
+        <li><input type="submit" name="signup"></li>
+        </form>
+        <a href="">Forgot Password</a>
+        </ul>
+        </div>
+        
+</div>
+
+
+
+<div class="signup">
+<form method="post" action="user-registration.php">
+<input type="hidden" name="submitted" value="true">
+<br>
+First Name<br>
+<input type="text" name="fname"><br>
+Last Name<br>
+<input type="text" name="lname"><br>
+E-mail<br>
+<input type="email" name="emailid"><br>
+Date of birth<br>
+<input type="date" name="dob"><br>
+Employee id<br>
+<input type="number" name="empid"><br>
+Mobile<br>
+<input type="number" name="phone"><br>
+Password<br>
+<input type="password" name="password"><br>
+Retype-Password<br>
+<input type="password" name="password"><br><br>
+<input type="checkbox" name="terms" /><br>
+Admin<br><br>
+<input type="checkbox" name="isadmin" /><br>
+I agree to the <a href="#" ><font color="#000000">terms &amp; conditions</font></a><br><br>                  
+<button type="submit"  value="submit" name="submit">Register</button>
+<?php
+echo "$newrec";
+?>
+</form>
+</div>
+
+<div class="footer">
+</div>
+</body>
+</html>
+                  
